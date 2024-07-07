9706effca774e6df5f57cdd702a88e9a88101ccc
adas1gupta <dasgupta1arjun@gmail.com>
2024-07-06 20:54:31 -0400

    successfully completed data visualization of large dataset (train.csv) from kaggle

--- web/app.py
+++ web/app.py
@@ -1,7 +1,8 @@
-from flask import Flask, jsonify, send_from_directory
+from flask import Flask, jsonify, send_from_directory, render_template
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, Float, Integer
+import pandas as pd
 import os
 
 app = Flask(__name__, static_folder='static', static_url_path='', template_folder='templates')
@@ -21,15 +22,32 @@ class Data(db.Model, Base):
         for key, value in kwargs.items():
             setattr(self, key, value)
 
-@app.route('/api/data')
-def get_data():
-    data = Data.query.all()
-    data_json = [{column.name: getattr(item, column.name) for column in item.__table__.columns} for item in data]
-    return jsonify(data_json)
-
 @app.route('/')
 def index():
-    return send_from_directory(app.static_folder, 'index.html')
+    return render_template('index.html')
+
+@app.route('/api/data')
+def get_data():
+    # Read the CSV file
+    csv_path = os.path.join(os.path.dirname(__file__), '..', 'tests', 'processed_train.csv')
+    df = pd.read_csv(csv_path)
+    
+    # Prepare data for visualization
+    trip_duration_data = df['trip_duration'].value_counts().sort_index()
+    passenger_count_data = df['passenger_count'].value_counts().sort_index()
+    
+    data = {
+        'trip_duration': {
+            'labels': trip_duration_data.index.tolist(),
+            'values': trip_duration_data.values.tolist()
+        },
+        'passenger_count': {
+            'labels': passenger_count_data.index.tolist(),
+            'values': passenger_count_data.values.tolist()
+        }
+    }
+    
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
